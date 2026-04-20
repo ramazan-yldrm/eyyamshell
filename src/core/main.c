b15b23dd295a72be726545c652b9cb430d499108
ramazan-yldrm <yldrmramazan056@gmail.com>
2026-04-20 08:57:52 +0300

    fix main

--- src/core/main.c
+++ src/core/main.c
@@ -5,21 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ryildiri <ryildiri@student.42kocaeli.com.t +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/02 18:16:28 by asari             #+#    #+#             */
-/*   Updated: 2026/01/10 16:12:34 by ryildiri         ###   ########.fr       */
+/*   Created: 2026/04/20 08:56:35 by ryildiri          #+#    #+#             */
+/*   Updated: 2026/04/20 08:56:35 by ryildiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <stdlib.h>
-#include "minishell.h" 
 #include "minishell.h"
-#include "libft.h"
 
 int	g_exit_status = 0;
-
-#include <string.h>
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -28,29 +24,11 @@ int	main(int argc, char **argv, char **envp)
 	t_cmd	*cmd;
 	t_env	*env_list;
 
+	(void)argc;
+	(void)argv;
 	setup_signals();
 	env_list = env_init(envp);
 	env_check_missing(&env_list);
-
-	// 🔥 BURASI YENİ: -c desteği
-	if (argc == 3 && strcmp(argv[1], "-c") == 0)
-	{
-		input = argv[2];
-
-		token = lexer(input);
-		if (token)
-		{
-			expander(token, env_list);
-			remove_quotes(token);
-			cmd = parser(token);
-			if (cmd)
-				executor(cmd, &env_list);
-		}
-		gc_free_type(GC_TEMP);
-		return (g_exit_status);
-	}
-
-	// 🔁 INTERACTIVE MOD (aynı kalıyor)
 	while (1)
 	{
 		input = readline("minishell$ ");
@@ -69,9 +47,7 @@ int	main(int argc, char **argv, char **envp)
 			remove_quotes(token);
 			cmd = parser(token);
 			if (cmd)
-			{		
 				executor(cmd, &env_list);
-			}
 		}
 		gc_free_type(GC_TEMP);
 	}
