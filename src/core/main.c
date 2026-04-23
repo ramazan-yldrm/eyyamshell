0ed59b5bedbc281ab0da914832dda4547477d230
ramazan-yldrm <yldrmramazan056@gmail.com>
2026-04-23 20:09:12 +0300

    fix main

--- src/core/main.c
+++ src/core/main.c
@@ -1,60 +1,75 @@
 #include "minishell.h"
-#include "libft.h"
 
-int g_signal = 0;
+int	g_signal = 0;
 
-int	main(int argc, char **argv, char **envp)
+static void	process_input(char *input, t_env **env)
+{
+	t_token	*tokens;
+	t_cmd	*cmd;
+
+	tokens = lexer(input);
+	if (!tokens)
+		return ;
+	expander(tokens, *env);
+	remove_empty_tokens(&tokens);
+	remove_quotes(tokens);
+	cmd = parser(tokens);
+	if (cmd)
+		executer(cmd, env);
+}
+
+static void	run_tester(char *cmd_str, t_env **env)
+{
+	process_input(cmd_str, env);
+	gc_free_type(GC_TEMP);
+}
+
+static void	handle_signal(void)
+{
+	if (g_signal == SIGINT)
+	{
+		get_set_status(1, 130);
+		g_signal = 0;
+	}
+}
+
+static void	interactive_loop(t_env **env)
 {
 	char	*input;
-	t_token	*token;
-	t_cmd	*cmd;
-	t_env	*env_list;
 
-	setup_signals();
-	env_list = env_init(envp);
-	env_check_missing(&env_list);
-
-	// 🔥 BURASI: -c desteği
-	if (argc == 3 && strcmp(argv[1], "-c") == 0)
-	{
-		input = argv[2];
-		token = lexer(input);
-		if (token)
-		{
-			expander(token, env_list);
-			remove_empty_tokens(&token);
-			remove_quotes(token);
-			cmd = parser(token);
-			if (cmd)
-				executer(cmd, &env_list);
-		}
-		gc_free_type(GC_TEMP);
-		return (get_set_status(0, 0));
-	}
-
-	// 🔁 INTERACTIVE MOD
 	while (1)
 	{
-		input = readline("minishell$ ");
+		setup_signals();
+		input = readline("minishell> ");
 		if (!input)
 		{
 			printf("exit\n");
 			cleanup_and_exit(get_set_status(0, 0));
 		}
+		handle_signal();
 		gc_add_node(input, GC_TEMP);
 		if (*input)
-			add_history(input);
-		token = lexer(input);
-		if (token)
 		{
-			expander(token, env_list);
-			remove_empty_tokens(&token);
-			remove_quotes(token);
-			cmd = parser(token);
-			if (cmd)
-				executer(cmd, &env_list);
+			add_history(input);
+			process_input(input, env);
 		}
 		gc_free_type(GC_TEMP);
 	}
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_env	*env_list;
+
+	env_list = NULL;
+	setup_signals();
+	env_list = env_init(envp);
+	env_check_missing(&env_list);
+	if (argc == 3 && !ft_strncmp(argv[1], "-c", 3))
+	{
+		run_tester(argv[2], &env_list);
+		return (get_set_status(0, 0));
+	}
+	interactive_loop(&env_list);
 	return (get_set_status(0, 0));
 }
